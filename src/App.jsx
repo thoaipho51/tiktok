797e9c2fd9871722d43c353b878ecba6e7984885
thoaipho51 <thoaipho51@gmail.com>
2025-03-08 22:39:12 +0700

    DefaultLayout css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from '~/routes';
-import { DefaultLayout , HeaderOnly} from './components/Layout';
+import { DefaultLayout, HeaderOnly } from './components/Layout';
 import { Fragment } from 'react';
-Fragment;
-DefaultLayout;
 const App = () => {
     return (
         <Router>
